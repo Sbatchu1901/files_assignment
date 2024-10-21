c13ca56027b1616b77b3dbe0877249e0c55e73d3
Sbatchu1901 <sbatchu@murraystate.edu>
2024-10-20 21:27:32 -0500

    final

--- load_json.py
+++ load_json.py
@@ -4,9 +4,9 @@ import pandas as pd
 
 def load_json_data():
     #print("Current directory:", os.getcwd())
-    folder_name = input("Enter the folder name where the JSON file is located: ") #inorderd to work with json file ... importing json and using pandas
+    folder_name = input("Enter the folder name where the JSON file is located at: ")
     file_name = 'sbatchu_adoptions.json'
-    file_path = os.path.join('OneDrive', 'Desktop', folder_name, file_name)# reading the file,loading the file into data variable 
+    file_path = os.path.join(folder_name, file_name)
     try:
         with open(file_path, 'r') as file:
             data = json.load(file)
@@ -19,11 +19,13 @@ def load_json_data():
         print("An unexpected error occurred")
 
 def csv_files(data, filename):
-    df = pd.DataFrame(data) #5 files ....data frames are converted into csv files by using this csv_files function
-    df.to_csv(filename, index=False)
+    df = pd.DataFrame(data)
+    path_to_directory = "C:\\Users\\sruja\\OneDrive\\Desktop\\files_assignment"
+    total_path = os.path.join(path_to_directory, filename)
+    df.to_csv(total_path, index=False)
 
 def contacts_file(data):
-    contacts = []   #seprating  contacts ...initializing the contacts for a particular record this is the function for 
+    contacts = []
     for item in data:
         if 'contacts' in item:
             for contact in item['contacts']:
@@ -32,10 +34,10 @@ def contacts_file(data):
     csv_files(contacts, 'contacts.csv')
 
 def universities_file(data):
-    universities = [] # we are initializinfg universties function....
+    universities = []
     for item in data :
-        universities.append(item['university']) # appending
-    csv_files(universities, 'universities.csv') #data frames into csv 
+        universities.append(item['university'])
+    csv_files(universities, 'universities.csv')
 
 def adoptions_file(data):
     adoptions = []
